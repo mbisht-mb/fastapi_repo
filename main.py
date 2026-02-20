c5e9737d0ccb82bdc7d4249bcdc5c214930e147d
mbisht-mb <mbisht@dons.usfca.edu>
2026-02-20 15:48:42 -0800

    Update greeting message in root endpoint

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Hello from FastAPI developed by Manjari Bisht on DigitalOcean for Enterprise Software Class! Wohooo!! Project launched!"}
+    return {"Hello from FastAPI developed by Manjari Bisht on DigitalOcean for Enterprise Software Class, Wohooo!! Project launched!"}
 
 
 @app.get("/health")
